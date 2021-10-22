4189ec6b03bcd093238e924e714ff68be0e0c488
丹下輝巳 <teru.0701.tt@gmail.com>
2021-10-22 09:56:18 +0900

    Timerクラスの追加

---
+++ Tank_Ojousama/DirectX12/Utility/Timer/Timer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Timer
+{
+public:
+	Timer(float t = 0.f);
+	~Timer();
+	void update();
+	void setTime(float t);
+	bool isTime(); const;
+	float rate()const;
+	float getCuttentTime()const;
+
+public:
+	static float mDeltaTime;
+
+private:
+	float mLimitTime;
+	float mCurrentTime;
+};
