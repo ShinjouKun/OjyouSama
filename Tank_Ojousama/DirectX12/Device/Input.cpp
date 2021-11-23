15d3d69acd1ea843361d0792333f19814591c133
丹下輝巳 <teru.0701.tt@gmail.com>
2021-11-23 16:37:54 +0900

    inputの更新 使えなくなるメソッドは全てassert処理をしているので、使えるメソッドに変更必要 ※金曜日に統合するのでそれまでマージしないこと

--- Tank_Ojousama/DirectX12/Device/Input.cpp
+++ Tank_Ojousama/DirectX12/Device/Input.cpp
@@ -1,6 +1,29 @@
 #include "Input.h"
 #include <cassert>
 
+#include "../Math/Math.h"
+#include <algorithm>
+
+#define SAFE_RELEASE(x) if(x){x->Release(); x=0;}
+
+BOOL CALLBACK enumJoysticksCallback(const DIDEVICEINSTANCE*, VOID*);
+BOOL CALLBACK enumObjectsCallback(const DIDEVICEOBJECTINSTANCE*, VOID*);
+BOOL CALLBACK enumFFDevicesCallback(const DIDEVICEINSTANCE*, VOID*);
+BOOL CALLBACK enumAxesCallback(const DIDEVICEOBJECTINSTANCE*, VOID*);
+
+BYTE Input::mCurrentKeys[256] = { 0 };
+BYTE Input::mPreviousKeys[256] = { 0 };
+DIJOYSTATE2 Input::mCurrentJoyState;
+DIJOYSTATE2 Input::mPreviousJoyState;
+LPDIRECTINPUT8 Input::mDinput = nullptr;
+LPDIRECTINPUTDEVICE8 Input::mKeyDevice = nullptr;
+LPDIRECTINPUTDEVICE8 Input::mPadDevice = nullptr;
+LPDIRECTINPUTEFFECT Input::mEffectDevice = nullptr;
+DWORD Input::mEffectNumForceedbackAxis;
+
+HWND Input::mHwnd;
+//
+
 ComPtr<IDirectInputDevice8> Input::devKeyboard = nullptr;
 BYTE Input::key[256] = { 0 };
 BYTE Input::oldkey[256] = { 0 };//前の状態
@@ -18,10 +41,50 @@ Input::Input()
 
 Input::~Input()
 {
+
+	if (mDinput)
+	{
+		SAFE_RELEASE(mDinput);
+	}
+
+	if (mKeyDevice)
+	{
+		SAFE_RELEASE(mKeyDevice);
+	}
+
+	if (mEffectDevice)
+	{
+		SAFE_RELEASE(mEffectDevice);
+	}
+
+	if (mPadDevice)
+	{
+		SAFE_RELEASE(mPadDevice);
+	}
 }
 
 void Input::Init(HWND hwnd)
 {
+
+
+	mHwnd = hwnd;
+
+	if (FAILED(keyBodeInit()))
+	{
+#ifdef _DEBUG
+		assert("キーボードの初期化失敗");
+#endif // DEBUG
+	}
+
+	if (FAILED(joyInit()))
+	{
+#ifdef _DEBUG
+		assert("ジョイスティックの初期化失敗");
+#endif // DEBUG
+	}
+
+	return;
+
 	HRESULT result;
 
 	HINSTANCE hInstance = GetModuleHandle(nullptr);
@@ -41,6 +104,28 @@ void Input::Init(HWND hwnd)
 
 void Input::Update()
 {
+	//1フレーム前の情報を上書き
+	memcpy_s(mPreviousKeys, sizeof(mPreviousKeys), mCurrentKeys, sizeof(mCurrentKeys));
+	mPreviousJoyState = mCurrentJoyState;
+
+
+	HRESULT hr = mKeyDevice->Acquire();
+	if ((hr == DI_OK) || (hr == S_FALSE)) {
+		mKeyDevice->GetDeviceState(sizeof(mCurrentKeys), &mCurrentKeys);
+	}
+
+	if (mPadDevice) {
+		hr = mPadDevice->Acquire();
+		if ((hr == DI_OK) || (hr == S_FALSE)) {
+			mPadDevice->GetDeviceState(sizeof(DIJOYSTATE2), &mCurrentJoyState);
+		}
+	}
+	else
+	{
+		joyInit();
+	}
+
+	return;
 	HRESULT result;
 	result = devKeyboard->Acquire();//キーボード情報のかくとく開始
 
@@ -56,8 +141,134 @@ void Input::Update()
 	result = devKeyboard->GetDeviceState(sizeof(key), key);
 }
 
+bool Input::getKeyDown(KeyCode key)
+{
+	return (mCurrentKeys[static_cast<BYTE>(key)] & 0x80 && !(mPreviousKeys[static_cast<BYTE>(key)] & 0x80));
+}
+
+bool Input::getKey(KeyCode key)
+{
+	return mCurrentKeys[static_cast<BYTE>(key)] & 0x80;
+}
+
+bool Input::getKeyUp(KeyCode key)
+{
+	return (!(mCurrentKeys[static_cast<BYTE>(key)] & 0x80) && mPreviousKeys[static_cast<BYTE>(key)] & 0x80);
+}
+
+bool Input::getJoyDown(JoyCode joy)
+{
+	return (mCurrentJoyState.rgbButtons[static_cast<int>(joy)] & 0x80 && !(mPreviousJoyState.rgbButtons[static_cast<int>(joy)] & 0x80));
+}
+
+bool Input::getJoy(JoyCode joy)
+{
+	return mCurrentJoyState.rgbButtons[static_cast<int>(joy)] & 0x80;
+}
+
+bool Input::getJoyUp(JoyCode joy)
+{
+	return (!(mCurrentJoyState.rgbButtons[static_cast<int>(joy)] & 0x80) && mPreviousJoyState.rgbButtons[static_cast<int>(joy)] & 0x80);
+}
+
+int Input::horizontal()
+{
+	if (getKey(KeyCode::A) /*|| getKey(KeyCode::LEFTARROW)*/) {
+		return -1;
+	}
+	else if (getKey(KeyCode::D) /*|| getKey(KeyCode::RIGHTARROW)*/) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int Input::ArrowHorizontal()
+{
+	if (getKey(KeyCode::LEFTARROW)) {
+		return -1;
+	}
+	else if (getKey(KeyCode::RIGHTARROW)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int Input::vertical()
+{
+	if (getKey(KeyCode::W) /*|| getKey(KeyCode::UPARROW)*/) {
+		return 1;
+	}
+	else if (getKey(KeyCode::S) /*|| getKey(KeyCode::DOWNARROW)*/) {
+		return -1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int Input::ArrowVertical()
+{
+	if (getKey(KeyCode::UPARROW)) {
+		return 1;
+	}
+	else if (getKey(KeyCode::DOWNARROW)) {
+		return -1;
+	}
+	else {
+		return 0;
+	}
+}
+
+float Input::joyHorizontal(float num)
+{
+	//最大1で返すため1000で割る
+	if (Math::abs(static_cast<float>(mCurrentJoyState.lX)) > num)
+	{
+		return mCurrentJoyState.lX / 1000.f;
+	}
+	return 0.f;
+}
+
+float Input::joyVertical(float num)
+{
+	//なぜか反転してるから
+	if (Math::abs(static_cast<float>(mCurrentJoyState.lY)) > num)
+	{
+		return -mCurrentJoyState.lY / 1000.f;
+	}
+	return 0.f;
+}
+
+float Input::joyRightHorizontal(float num)
+{
+	//最大1で返すため1000で割る
+	if (Math::abs(static_cast<float>(mCurrentJoyState.lRx)) > num)
+	{
+		return mCurrentJoyState.lRx / 1000.f;
+	}
+	return 0.f;
+}
+
+float Input::joyRightVertical(float num)
+{
+	//なぜか反転してるから
+	if (Math::abs(static_cast<float>(mCurrentJoyState.lRy)) > num)
+	{
+		return -mCurrentJoyState.lRy / 1000.f;
+	}
+	return 0.f;
+}
+
 bool Input::KeyDown(int keyNum)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return false;
 	if (keyNum < 0x00)return false;//エラー対策
 	if (keyNum > 0xff)return false;//エラー対策
 	if (!oldkey[keyNum] && key[keyNum])
@@ -69,6 +280,10 @@ bool Input::KeyDown(int keyNum)
 
 bool Input::KeyState(int keyNum)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return false;
 	if (keyNum < 0x00)return false;//エラー対策
 	if (keyNum > 0xff)return false;//エラー対策
 	if (key[keyNum])
@@ -99,6 +314,10 @@ BOOL CALLBACK DeviceFindCallBack(LPCDIDEVICEINSTANCE ipddi, LPVOID pvRef)
 
 bool Input::InitGamepad(HWND hwnd)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return true;
 	HRESULT result;
 
 
@@ -183,6 +402,11 @@ bool Input::InitGamepad(HWND hwnd)
 
 void Input::UpdateGamepad()
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+
+	return;
 	HRESULT result;
 	memcpy(buttonPre, pad_data.rgbButtons, sizeof(pad_data.rgbButtons));
 
@@ -199,6 +423,10 @@ void Input::UpdateGamepad()
 
 bool Input::PushButton(BYTE buttonNumber)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return false;
 	// 異常な引数を検出
 	//assert(0 <= buttonNumber && buttonNumber <= 32);
 
@@ -213,6 +441,10 @@ bool Input::PushButton(BYTE buttonNumber)
 
 bool Input::TriggerButton(BYTE buttonNumber)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return false;
 	// 異常な引数を検出
 	//assert(0 <= buttonNumber && buttonNumber <= 32);
 
@@ -227,6 +459,10 @@ bool Input::TriggerButton(BYTE buttonNumber)
 
 bool Input::LeftStickTilt(LONG stickNumber)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return false;
 	//　未実装
 
 	//　無反応範囲
@@ -256,6 +492,10 @@ bool Input::LeftStickTilt(LONG stickNumber)
 
 bool Input::RightStickTilt(LONG stickNumber)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return false;
 	//　無反応範囲
 	DWORD unresponsive_range = 0;
 
@@ -282,6 +522,10 @@ bool Input::RightStickTilt(LONG stickNumber)
 
 bool Input::CrossButton(LONG buttonData)
 {
+#ifdef _DEBUG
+	assert("現在使えないので、変えておいてね");
+#endif // DEBUG
+	return false;
 	//　未実装
 
 	if (pad_data.rgdwPOV[0] != buttonData)
@@ -295,4 +539,154 @@ bool Input::CrossButton(LONG buttonData)
 
 
 	return false;
+}
+
+HRESULT Input::keyBodeInit()
+{
+	// 「DirectInput」オブジェクトの作成
+	if (FAILED(DirectInput8Create(GetModuleHandle(NULL), DIRECTINPUT_VERSION, IID_IDirectInput8, (VOID**)&mDinput, NULL))) {
+		return E_FAIL;
+	}
+
+	// 「DirectInputデバイス」オブジェクトの作成
+	if (FAILED(mDinput->CreateDevice(GUID_SysKeyboard, &mKeyDevice, NULL))) {
+		return E_FAIL;
+	}
+	// デバイスをキーボードに設定
+	if (FAILED(mKeyDevice->SetDataFormat(&c_dfDIKeyboard))) {
+		return E_FAIL;
+	}
+	// 協調レベルの設定
+	if (FAILED(mKeyDevice->SetCooperativeLevel(mHwnd, DISCL_NONEXCLUSIVE | DISCL_FOREGROUND))) {
+		return E_FAIL;
+	}
+	// デバイスを「取得」する
+	mKeyDevice->Acquire();
+	return S_OK;
+}
+
+HRESULT Input::joyInit()
+{
+	//利用可能なゲームコントローラーの列挙関数を実行
+	if (FAILED(mDinput->EnumDevices(DI8DEVCLASS_GAMECTRL, enumJoysticksCallback, NULL, DIEDFL_ATTACHEDONLY))) {
+		return E_FAIL;
+	}
+	if (!mPadDevice) { //パッドの接続はせずに終了
+		return S_OK;
+	}
+	// デバイスをジョイスティックに設定
+	if (FAILED(mPadDevice->SetDataFormat(&c_dfDIJoystick2))) {
+		return E_FAIL;
+	}
+	// 協調レベルの設定
+	if (FAILED(mPadDevice->SetCooperativeLevel(mHwnd, DISCL_NONEXCLUSIVE | DISCL_FOREGROUND))) {
+		return E_FAIL;
+	}
+	//アプリケーションで使用するコントローラーのプロパティを列挙して設定する
+	//if (FAILED(mPadDevice->EnumObjects(enumObjectsCallback, NULL, DIDFT_ALL))) {
+	//	return E_FAIL;
+	//}
+	if (FAILED(mPadDevice->EnumObjects(enumObjectsCallback, (void*)mEffectNumForceedbackAxis, DIDFT_AXIS))) {
+		return E_FAIL;
+	}
+
+	//エフェクトの軸と方向をセット
+	DWORD rgdwAxes[2] = { DIJOFS_X,DIJOFS_Y };
+	LONG rglDirection[2] = { 1,1 };//この値を変化させるとバランス調整ができる
+
+	//構造体
+	DICONSTANTFORCE cf;
+	DIEFFECT eff;
+
+	ZeroMemory(&eff, sizeof(eff));
+	eff.dwSize = sizeof(DIEFFECT);
+	eff.dwFlags = DIEFF_CARTESIAN | DIEFF_OBJECTOFFSETS;
+	eff.dwDuration = INFINITE;
+	eff.dwSamplePeriod = 0;
+	eff.dwGain = DI_FFNOMINALMAX;
+	eff.dwTriggerButton = DIEB_NOTRIGGER;
+	eff.dwTriggerRepeatInterval = 0;
+	eff.cAxes = mEffectNumForceedbackAxis;
+	eff.rgdwAxes = rgdwAxes;
+	eff.rglDirection = rglDirection;
+	eff.lpEnvelope = 0;//
+	eff.cbTypeSpecificParams = sizeof(DICONSTANTFORCE);
+	eff.lpvTypeSpecificParams = &cf;
+	eff.dwStartDelay = 0;
+
+
+	if (FAILED(mPadDevice->CreateEffect(GUID_ConstantForce, &eff, &mEffectDevice, NULL)))
+	{
+		return E_FAIL;
+	}
+
+	// デバイスを「取得」する
+	mPadDevice->Acquire();
+
+	return S_OK;
+}
+
+//コールバック
+
+BOOL CALLBACK enumJoysticksCallback(const DIDEVICEINSTANCE* pdidInstance, VOID* pContext) {
+	//複数列挙される場合、ユーザーに選択・確認させる
+	WCHAR szConfirm[MAX_PATH];
+	//↓で使用可能パッドを列挙するが、今は最初の一つで初期化
+	//wsprintf(szConfirm, L"この物理デバイスでデバイスオブジェクトを作成しますか？\n商品名%s\nインスタンス名%s", pdidInstance->tszProductName, pdidInstance->tszInstanceName);
+	//if (MessageBox(0, szConfirm, L"確認", MB_YESNO) == IDNO) {
+	//    return DIENUM_CONTINUE;
+	//}
+	// 「DirectInputデバイス」オブジェクトの作成
+	if (FAILED(Input::mDinput->CreateDevice(pdidInstance->guidInstance, &Input::mPadDevice, NULL))) {
+		return DIENUM_CONTINUE;
+	}
+
+	return DIENUM_STOP;
+}
+
+BOOL CALLBACK enumObjectsCallback(const DIDEVICEOBJECTINSTANCE* pdidoi, VOID* pContext) {
+	if (pdidoi->dwType & DIDFT_AXIS) {
+		DIPROPRANGE diprg;
+		diprg.diph.dwSize = sizeof(DIPROPRANGE);
+		diprg.diph.dwHeaderSize = sizeof(DIPROPHEADER);
+		diprg.diph.dwHow = DIPH_BYID;
+		diprg.diph.dwObj = pdidoi->dwType;
+		diprg.lMin = -1000;
+		diprg.lMax = 1000;
+
+		if (FAILED(Input::mPadDevice->SetProperty(DIPROP_RANGE, &diprg.diph))) {
+			return DIENUM_STOP;
+		}
+	}
+	return DIENUM_CONTINUE;
+}
+
+BOOL CALLBACK enumFFDevicesCallback(const DIDEVICEINSTANCE* pInput, VOID* pContext)
+{
+	HRESULT hr;
+
+	hr = Input::mDinput->CreateDevice(pInput->guidInstance, &Input::mPadDevice, NULL);
+	if (FAILED(hr))return DIENUM_CONTINUE;
+
+	return DIENUM_STOP;
+}
+
+BOOL CALLBACK enumAxesCallback(const DIDEVICEOBJECTINSTANCE* pdidoi, VOID* pContext)
+{
+	HRESULT hr;
+	DIPROPRANGE diprg;
+	diprg.diph.dwSize = sizeof(DIPROPRANGE);
+	diprg.diph.dwHeaderSize = sizeof(DIPROPHEADER);
+	diprg.diph.dwHow = DIPH_BYID;
+	diprg.diph.dwObj = pdidoi->dwType;
+	diprg.lMin = -1000;
+	diprg.lMax = 1000;
+	hr = Input::mPadDevice->SetProperty(DIPROP_RANGE, &diprg.diph);
+
+	if (FAILED(hr))return DIENUM_STOP;
+
+	DWORD* pdwNumForceFeedbackAxis = (DWORD*)pContext;
+	if ((pdidoi->dwFlags&DIDOI_FFACTUATOR) != 0)(*pdwNumForceFeedbackAxis)++;
+
+	return DIENUM_CONTINUE;
 }
