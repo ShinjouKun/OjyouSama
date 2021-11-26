f61b4b280f8d30115edf0e0076960482e517756d
しんじょーくん <50821568+ShinjouKun@users.noreply.github.com>
2021-11-26 09:23:09 +0900

    デバッグ時の処理追加

--- Tank_Ojousama/DirectX12/Device/DirectXManager.cpp
+++ Tank_Ojousama/DirectX12/Device/DirectXManager.cpp
@@ -645,7 +645,7 @@ void DirectXManager::PostEffctBegin()
 		cmdList->ClearRenderTargetView(handles[i], clearColor, 0, nullptr);//ここくさい
 	}
 
-//#ifdef DEBUG
+#ifdef _DEBUG
 
 	//imgui関連
 	ImGui_ImplDX12_NewFrame();
@@ -654,13 +654,13 @@ void DirectXManager::PostEffctBegin()
 	ImGui::Begin("Debug Window 1");
 	ImGui::SetWindowSize(ImVec2(400, 500), ImGuiCond_::ImGuiCond_FirstUseEver);
 
-//#endif  DEBUG
+#endif  _DEBUG
 
 }
 
 void DirectXManager::PostEffctEnd()
 {
-//#ifdef DEBUG
+#ifdef _DEBUG
 
 	ImGui::End();
 	ImGui::Render();
@@ -668,7 +668,7 @@ void DirectXManager::PostEffctEnd()
 	cmdList->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
 	ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), cmdList.Get());
 
-//#endif  DEBUG
+#endif  _DEBUG
 
 
 	//リソースバリアを戻す
